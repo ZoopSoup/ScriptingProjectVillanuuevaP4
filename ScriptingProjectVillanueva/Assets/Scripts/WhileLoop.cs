5f3dbd18f7ce7f8b4e08c53a5bd9be7f00456887
ZoopSoup <zoopidysoupidy@gmail.com>
2022-11-02 11:42:30 -0700

    Created and Completed LoopingTime
    Created and completed all 4 parts of LoopingTime Scripting project

--- ScriptingProjectVillanueva/Assets/Scripts/WhileLoop.cs
+++ ScriptingProjectVillanueva/Assets/Scripts/WhileLoop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Loops : MonoBehaviour
+public class WhileLoop : MonoBehaviour
 {
     int cupsInTheSink = 4;
 
