7d688478dc573b52465fd1f2504ec41bca16f51f
ZoopSoup <zoopidysoupidy@gmail.com>
2023-03-09 10:58:23 -0800

    Completed OnMouseDown
    Completed the entirety of OnMouseDown

---
+++ ScriptingProjectVillanueva/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+    // In this case, result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);\
+    // The Mathf.Lerp function takes 3 float parameters: one representing the value to interpolate from; another representing the value to interpolate to and a final float representing how far to interpolate.
+
+    Vector3 from = new Vector3(1f, 2f, 3f);
+    Vector3 to = new Vector3(5f, 6f, 7f);
+
+    // Here result - (4, 5, 6)
+    Vector 3 result = Vector3.Lerp (from, to, 0.75f);
+    // Vector3 works the same as mathf.lerp but the 'from' and 'to' values are of type Color and Vector3 respectively.
+
+    void Update ()
+    {
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+    }
+    // If the intensity starts off at 0 then after the first update it will be set to 4. The next frame it will be set to 6, then to 7, then to 7.5 and so on.
+
+    void Update ()
+    {
+        light.intensity = Mathf.Lerp(light.intensity, 8f 0.5f * Time.deltaTime);
+    // This would mean the change to intensity would happen per second instead of per frame.
+    */
+    }
